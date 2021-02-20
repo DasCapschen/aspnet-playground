7d4947a1e1fa89e53049a93d341641a10602f527
DasCapschen <3923406+DasCapschen@users.noreply.github.com>
2021-02-20 10:42:47 +0100

    configure ActivityProtocol relationship

--- src/Data/ApplicationDbContext.cs
+++ src/Data/ApplicationDbContext.cs
@@ -1,8 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using src.Models;
 
 namespace src.Data
 {
@@ -12,5 +14,18 @@ namespace src.Data
             : base(options)
         {
         }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<src.Models.ActivityProtocol.ProtocolEntry>()
+                .HasOne(e => e.Protocol) //configure that entry must have *one* protocol
+                .WithMany(p => p.Entries) //configure that protocol may have *many* entries
+                .IsRequired(); //foreign key may NOT be NULL!
+        }
+
+        public DbSet<src.Models.ActivityProtocol.ProtocolEntry> ProtocolEntries { get; set; }
+        public DbSet<src.Models.ActivityProtocol> ActivityProtocols { get; set; }
     }
 }
