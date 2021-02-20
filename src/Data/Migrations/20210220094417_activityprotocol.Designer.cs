89012c59b0609d2e66942306ce4bd66f97bd10fb
DasCapschen <3923406+DasCapschen@users.noreply.github.com>
2021-02-20 10:44:58 +0100

    add migrations, update db

--- src/Data/Migrations/20210220094417_activityprotocol.Designer.cs
+++ src/Data/Migrations/20210220094417_activityprotocol.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using src.Data;
 
 namespace src.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210220094417_activityprotocol")]
+    partial class activityprotocol
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
