1e38af697dbc422770b3cf891cfc190fd268c01b
DasCapschen <3923406+DasCapschen@users.noreply.github.com>
2021-02-21 17:43:15 +0100

    update code to use postgres

--- src/Startup.cs
+++ src/Startup.cs
@@ -27,9 +27,8 @@ namespace src
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlite(
-                    Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<ApplicationDbContext>(options => 
+                options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
 
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
