b991e7be8064da4cae7d785292dbd9082553e9f1
DasCapschen <3923406+DasCapschen@users.noreply.github.com>
2021-02-25 09:08:12 +0000

    utilize vscode docker remote-container :)

--- README.md
+++ README.md
@@ -7,19 +7,34 @@ Dockers needs to be set up for this project to work.
 All the building and running the project will happen inside docker containers.  
 Internet access is required first time to pull the necessary docker images.  
 
-## Building
+## NEW WORKFLOW
+You need to install the vscode extension [remote-containers](https://marketplace.visualstudio.com/items?itemName=ms-vscode-remote.remote-containers).  
+It should then automatically find the files in .devcontainer folder and ask you if you want to open the project in the docker container, do so.  
+Now you can open a new terminal in vs code and access the command line to build the project etc. (or launch through vscode, but I didn't test if launch.json works)  
+
+To build: `./project.sh build` or `./project.sh rebuild`  
+To run: `./project.sh start`  
+To stop: ctrl+c, you're using a terminal ;p  
+(project.sh will automatically figure out if it is runnning in a dev container or locally)  
+
+dotnet ef and dotnet aspnet-codegeneration are installed in the devcontainer.
+
+You can access the website on localhost:8182
+
+## OLD WORKFLOW
+### Building
 Run `./project.sh build` to build the project, 
 or run `./project.sh rebuild` to clean-build it.  
 
-## Code Generation
+### Code Generation
 If you need to generate Code, or do anything else that requires the `dotnet` command,
 you can run `./project.sh shell` to start a bash shell with dotnet installed (runs in docker).  
 After you started the shell, the first command you should run is `dotnet restore`   
 The tools `dotnet ef` and `dotnet aspnet-codegeneration` are also installed.
 
-## Running
+### Running
 Run `./project.sh start` to start the default kestrel webserver.  
-You can access the website on localhost:8080  
+You can access the website on localhost:8182  
 Stop the webserver using `./project.sh stop`  
 
 If you want to quickly see your code-changes, you can run `./project.sh restart`.
--- project.sh
+++ project.sh
@@ -1,31 +1,49 @@
 #!/bin/bash
 
 build() {
-    #build the image (see Dockerfile)
-    #target is run (we have 2 stages, run and build)
-    #name the created image "aspnet-playground"
-    docker build --target run -t aspnet-playground -f ./docker/aspnet-playground/Dockerfile .
+    if [ $IS_DEV_CONTAINER = 1 ]; then
+        dotnet build src/src.csproj
+        dotnet publish src/src.csproj
+    else
+        #build the image (see Dockerfile)
+        #target is run (we have 2 stages, run and build)
+        #name the created image "aspnet-playground"
+        docker build --target run -t aspnet-playground -f ./docker/aspnet-playground/Dockerfile .
+    fi
 }
 
 rebuild() {
-    docker build --target run -t aspnet-playground --no-cache -f ./docker/aspnet-playground/Dockerfile .
+    if [ $IS_DEV_CONTAINER = 1 ]; then
+        dotnet clean src/src.csproj
+        build
+    else
+        docker build --target run -t aspnet-playground --no-cache -f ./docker/aspnet-playground/Dockerfile .
+    fi
 }
 
 start() {
-    #run in background (-d)
-    #forward HOST port 8182 to CONTAINER port 80
-    #run image called "aspnet-playground"
-    #remove container when stopped (--rm)
-    #docker run --name aspnet-playground --rm -p 8182:80 -d aspnet-playground
-    docker-compose -f ./docker/docker-compose.yml up -d web db
+    if [ $IS_DEV_CONTAINER = 1 ]; then
+        dotnet run --project src/src.csproj
+    else
+        #run in background (-d)
+        #forward HOST port 8182 to CONTAINER port 80
+        #run image called "aspnet-playground"
+        #remove container when stopped (--rm)
+        #docker run --name aspnet-playground --rm -p 8182:80 -d aspnet-playground
+        docker-compose -f ./docker/docker-compose.yml up -d web db
+    fi
 }
 
 stop() {
+    #no need inside dev container, just use CTRL+C, you're running from terminal anyways :)
+
     #docker stop aspnet-playground
     docker-compose -f ./docker/docker-compose.yml down
 }
 
 shell() {
+    #unnecessary when using dev container :)
+
     docker build --build-arg UID=$(id -u) --build-arg GID=$(id -g) -t aspnet-playground-dotnet-shell -f ./docker/dotnet-shell/Dockerfile .
     docker-compose -f ./docker/docker-compose.yml run --rm dotnet_shell
 }
@@ -38,17 +56,18 @@ cd $PROJECT_ROOT
 
 # letting docker-compose or similar pull the images results in a "connection refused"
 # for some reason, so we have to explicitly pull the images before
+if ! [ $IS_DEV_CONTAINER = 1 ]; then
+    if [[ "$(docker images -q mcr.microsoft.com/dotnet/sdk:5.0 2> /dev/null)" == "" ]]; then
+    docker pull mcr.microsoft.com/dotnet/sdk:5.0
+    fi
 
-if [[ "$(docker images -q mcr.microsoft.com/dotnet/sdk:5.0 2> /dev/null)" == "" ]]; then
-  docker pull mcr.microsoft.com/dotnet/sdk:5.0
-fi
+    if [[ "$(docker images -q mcr.microsoft.com/dotnet/sdk:5.0 2> /dev/null)" == "" ]]; then
+    docker pull mcr.microsoft.com/dotnet/aspnet:5.0
+    fi
 
-if [[ "$(docker images -q mcr.microsoft.com/dotnet/sdk:5.0 2> /dev/null)" == "" ]]; then
-  docker pull mcr.microsoft.com/dotnet/aspnet:5.0
-fi
-
-if [[ "$(docker images -q postgres:13.2-alpine 2> /dev/null)" == "" ]]; then
-  docker pull postgres:13.2-alpine
+    if [[ "$(docker images -q postgres:13.2-alpine 2> /dev/null)" == "" ]]; then
+    docker pull postgres:13.2-alpine
+    fi
 fi
 
 case "$1" in
